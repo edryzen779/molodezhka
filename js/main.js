c1b7b4dca1b1c904f94f53d325b676e3f245a012
edryzen779 <93119952+edryzen779@users.noreply.github.com>
2021-11-08 11:28:23 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,6 +10,12 @@ window.addEventListener("scroll",function(){
 	const header = document.querySelector('header');
 	header.classList.toggle('sticky', window.scrollY>0);
 });
+const navigation = document.querySelector('nav');
+document.querySelector('.toggle').onclick = function(){
+	this.classList.toggle('active');
+	navigation.classList.toggle('active'); 
+}
+
 
 const btns = document.querySelectorAll('.back__btn');
 const modalOverlay = document.querySelector('.back__overlay ');
