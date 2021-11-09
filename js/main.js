667ff34ead6adde637c673f784a7be1c8ab72212
edryzen779 <93119952+edryzen779@users.noreply.github.com>
2021-11-09 22:54:35 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,21 +6,35 @@ $(".gallery-list").magnificPopup({
 	}
 });
 
+const header = document.querySelector('header');
+const body = document.querySelector('body');
+const navigation = document.querySelector('nav');
+const toggle = document.querySelector('.toggle');
+
+
 window.addEventListener("scroll", function () {
-	const header = document.querySelector('header');
 	header.classList.toggle('sticky', window.scrollY > 0);
 });
-const navigation = document.querySelector('nav');
+
 document.querySelector('.toggle').onclick = function () {
 	this.classList.toggle('active');
+	body.classList.toggle('lock-scroll');
 	navigation.classList.toggle('active');
-	if ($('body').hasClass('lock-scroll')) {
-		$('body').removeClass('lock-scroll');
-	}
-	else {
-		$('body').addClass('lock-scroll');
+	header.classList.add('sticky');
+
+	// if ($('body').hasClass('lock-scroll')) {
+	// 	$('body').removeClass('lock-scroll');
+	// }
+	// else {
+	// 	$('body').addClass('lock-scroll');
+	// };
+	document.querySelector('.header__nav-items').onclick = function () {
+		navigation.classList.toggle('active');
+		toggle.classList.toggle('active')
+		body.classList.toggle('lock-scroll');
 	}
 }
+
 
 
 const btns = document.querySelectorAll('.back__btn');
@@ -50,3 +64,62 @@ modalOverlay.addEventListener('click', (e) => {
 		});
 	}
 });
+
+
+$(document).ready(function () {
+	if ($(window).width() > 992) {
+		$('.menu__items').slick({
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 3000,
+			variableWidth: true,
+			rows: 2,
+			dots: true,
+			infinite: true
+		}
+		)
+	}
+	else if ($(window).width() > 590) {
+		$('.menu__items').slick({
+			slidesToShow: 2,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 3000,
+			variableWidth: true,
+			dots: true,
+			rows: 2,
+			infinite: true
+		})
+	}
+	else if ($(window).width() < 590){
+		$('.menu__items').slick({
+			centerMode: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 3000,
+			variableWidth: true,
+			dots: true,
+			rows: 4,
+			infinite: true
+		})
+	}
+});
+
+// $(window).resize(function () {
+// 	if (window.width() < 768) {
+// 		$('.menu__items').slick({
+// 			slidesToShow: 3,
+// 			slidesToScroll: 1,
+// 			autoplay: true,
+// 			autoplaySpeed: 3000,
+// 			variableWidth: true,
+// 			rows: 3,
+// 			speed: 500
+// 		})
+// 	}
+// 	else {
+// 		//You probably want to do something here...
+// 	}
+// });
